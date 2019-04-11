e17d6d0e71ea0565c99fe8107b6c68edf6ebe17c
James Manners <james@manners.net.au>
2019-04-11 20:10:08 +1000

    Improved typing of HttpWarmup middleware

--- lib/http-warmup.ts
+++ lib/http-warmup.ts
@@ -1,6 +1,8 @@
+import { Middleware } from "koa";
+
 const WARMUP_HEADER = "x-serverless-warmup";
 
-export const HttpWarmup = (ctx, next) => {
+export const HttpWarmup: Middleware = (ctx, next) => {
   if (ctx.request.headers[WARMUP_HEADER] === "true") {
     ctx.response.body = undefined;
     return;
